bd2ac0d4d5b5156b4fa056451728281d25e9bfd6
Rohit Deshmukh <112280707+drohit02@users.noreply.github.com>
2024-03-04 15:36:04 +0530

    Add files via upload

---
+++ DataType/JS/datatypes.js
@@ -0,0 +1,16 @@
+/* Assignment 2 : Data Types.
+    1. Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+    2. Log the types of isIsland, population, country and language to the console.
+*/
+let country = "Bharat";
+let continent = "Asia";
+let population = 1400000000;
+
+let isIsland = false;
+let language;
+
+console.log("Data Type of country is : " + typeof country);
+console.log("Data Type of population is : " + typeof population);
+console.log("Data Type of continent is : " + typeof continent);
+console.log("Data Type of isIsland is " + typeof isIsland);
+console.log("Data Type of language is : " + typeof language);
