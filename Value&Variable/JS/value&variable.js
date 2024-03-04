ac42e384e70563ca13d5e034c5ebe5ac96453e8d
Rohit Deshmukh <112280707+drohit02@users.noreply.github.com>
2024-03-04 13:41:42 +0530

    Add files via upload

---
+++ Value&Variable/JS/value&variable.js
@@ -0,0 +1,12 @@
+/* Assignment 1 : Value and Variables 
+Statement : Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console.
+*/
+let country = "Bharat";
+let continent = "Asia";
+let population = 14000000000;
+
+console.log("Country Name : " + country);
+console.log("Continent Name : " + continent);
+console.log("Population of country " + country + " is : " + population);
